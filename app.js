3b84f8a3c12a796220cde53a1894d6f5282f18be
Ezzfish <421929659@qq.com>
2020-05-20 10:52:11 +0800

    跨域

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ var Mock = require('mockjs')
 
 var app = express()
 
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8")
+    next()
+})  
+
 app.all('/', function (req, res) {
-    res.json(Mock.mock({
+    res.json(
+        123
+        
+        /*
+        Mock.mock({
         "data|12":[{
             "month|+1":1,
             "value|0-500":20
-        }]
-    }))
+            }]
+        })
+        */
+    
+    )
 })
 
-app.listen('8000')
+
+
+
+var server= app.listen('8000')
